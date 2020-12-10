0c008682e2d68c2e33233a38e2bd1097cce8a599
Philip Christiansen <philipchristiansen@gmail.com>
2020-12-10 14:05:07 +0100

    Input types experiment

--- src/vnodes.ts
+++ src/vnodes.ts
@@ -14,10 +14,8 @@ export interface AnchorAttributes extends NodeAttributes {
     href?: string;
 }
 
-type HtmlInputKey = Extract<keyof HTMLInputElement, string>;
-
-type InputAttributes = {
-    [key in HtmlInputKey]?: Subscribable<string> | string;
+export interface InputAttributes extends NodeAttributes {
+    value: Subscribable<string> | string;
 }
 
 export interface NodeAttributes extends Partial<GlobalEventHandlers> {
@@ -71,54 +69,47 @@ export function n(tag: string | CustomTagFunction, ...args: any[]): VElement {
 
 const nn = (tag: string) => (...args: any[]) => ni(tag, args);
 
-type TagOverloads = {
-    (attributes: NodeAttributes): VElement;
+type TagOverloads<A> = {
+    (attributes: A): VElement;
     (children: Child[]): VElement;
     (...children: Child[]): VElement;
-    (attributes: NodeAttributes, children: Child[]): VElement;
-    (attributes: NodeAttributes, ...children: Child[]): VElement;
-}
-type InputOverloads = {
-    (attributes: InputAttributes): VElement;
-    (children: Child[]): VElement;
-    (...children: Child[]): VElement;
-    (attributes: InputAttributes, children: Child[]): VElement;
-    (attributes: InputAttributes, ...children: Child[]): VElement;
+    (attributes: A, children: Child[]): VElement;
+    (attributes: A, ...children: Child[]): VElement;
 }
 
-export const a: TagOverloads = nn('a');
-export const b: TagOverloads = nn('b');
-export const body: TagOverloads = nn('body');
-export const br: TagOverloads = nn('br');
-export const button: TagOverloads = nn('button');
-export const canvas: TagOverloads = nn('canvas');
-export const code: TagOverloads = nn('code');
-export const div: TagOverloads = nn('div');
-export const form: TagOverloads = nn('form');
-export const h1: TagOverloads = nn('h1');
-export const h2: TagOverloads = nn('h2');
-export const h3: TagOverloads = nn('h3');
-export const h4: TagOverloads = nn('h4');
-export const h5: TagOverloads = nn('h5');
-export const h6: TagOverloads = nn('h6');
-export const img: TagOverloads = nn('img');
-export const input: InputOverloads = nn('input');
-export const label: TagOverloads = nn('label');
-export const legend: TagOverloads = nn('legend');
-export const li: TagOverloads = nn('li');
-export const meta: TagOverloads = nn('meta');
-export const ol: TagOverloads = nn('ol');
-export const option: TagOverloads = nn('option');
-export const p: TagOverloads = nn('p');
-export const span: TagOverloads = nn('span');
-export const select: TagOverloads = nn('select');
-export const table: TagOverloads = nn('table');
-export const tbody: TagOverloads = nn('tbody');
-export const td: TagOverloads = nn('td');
-export const textarea: TagOverloads = nn('textarea');
-export const th: TagOverloads = nn('th');
-export const thead: TagOverloads = nn('thead');
-export const title: TagOverloads = nn('title');
-export const tr: TagOverloads = nn('tr');
-export const ul: TagOverloads = nn('ul');
-export const video: TagOverloads = nn('video');
+export const a: TagOverloads<NodeAttributes> = nn('a');
+export const b: TagOverloads<NodeAttributes> = nn('b');
+export const body: TagOverloads<NodeAttributes> = nn('body');
+export const br: TagOverloads<NodeAttributes> = nn('br');
+export const button: TagOverloads<NodeAttributes> = nn('button');
+export const canvas: TagOverloads<NodeAttributes> = nn('canvas');
+export const code: TagOverloads<NodeAttributes> = nn('code');
+export const div: TagOverloads<NodeAttributes> = nn('div');
+export const form: TagOverloads<NodeAttributes> = nn('form');
+export const h1: TagOverloads<NodeAttributes> = nn('h1');
+export const h2: TagOverloads<NodeAttributes> = nn('h2');
+export const h3: TagOverloads<NodeAttributes> = nn('h3');
+export const h4: TagOverloads<NodeAttributes> = nn('h4');
+export const h5: TagOverloads<NodeAttributes> = nn('h5');
+export const h6: TagOverloads<NodeAttributes> = nn('h6');
+export const img: TagOverloads<NodeAttributes> = nn('img');
+export const input: TagOverloads<InputAttributes> = nn('input');
+export const label: TagOverloads<NodeAttributes> = nn('label');
+export const legend: TagOverloads<NodeAttributes> = nn('legend');
+export const li: TagOverloads<NodeAttributes> = nn('li');
+export const meta: TagOverloads<NodeAttributes> = nn('meta');
+export const ol: TagOverloads<NodeAttributes> = nn('ol');
+export const option: TagOverloads<NodeAttributes> = nn('option');
+export const p: TagOverloads<NodeAttributes> = nn('p');
+export const span: TagOverloads<NodeAttributes> = nn('span');
+export const select: TagOverloads<NodeAttributes> = nn('select');
+export const table: TagOverloads<NodeAttributes> = nn('table');
+export const tbody: TagOverloads<NodeAttributes> = nn('tbody');
+export const td: TagOverloads<NodeAttributes> = nn('td');
+export const textarea: TagOverloads<NodeAttributes> = nn('textarea');
+export const th: TagOverloads<NodeAttributes> = nn('th');
+export const thead: TagOverloads<NodeAttributes> = nn('thead');
+export const title: TagOverloads<NodeAttributes> = nn('title');
+export const tr: TagOverloads<NodeAttributes> = nn('tr');
+export const ul: TagOverloads<NodeAttributes> = nn('ul');
+export const video: TagOverloads<NodeAttributes> = nn('video');
